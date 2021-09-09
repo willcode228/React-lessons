bb8ad4e3b7ba11adaaf148128dd56ff01ba91756
Ilchenko Daniil Yurievich <crocus161@gmail.com>
2021-09-09 19:21:20 +0300

    Update project to rebegin it

--- src/components/Nav/Friend/Friend.jsx
+++
@@ -1,9 +0,0 @@
-import style from './Friend.module.css';
-
-function Friend(props) {
-    return (<div className={style.box}>
-                <h3 className={style.title}>{props.name}</h3>
-            </div>);
-}
-
-export default Friend;
--- src/components/Profile/MyPosts/Post/Posts.jsx
+++
@@ -1,6 +0,0 @@
-const Post = (props) => {
-    return (
-    <p className="post"><span className="repl">-</span>{props.message}</p>
-    );
-}
-export default Post;
