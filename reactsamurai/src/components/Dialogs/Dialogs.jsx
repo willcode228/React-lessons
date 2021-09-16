1ec17482ab036939000bfa79c2078806df910c76
Ilchenko Daniil Yurievich <crocus161@gmail.com>
2021-09-16 20:44:04 +0300

    Add Dialogs screen

---
+++ reactsamurai/src/components/Dialogs/Dialogs.jsx
@@ -0,0 +1,11 @@
+import { Main } from "../StyledComponents/Main";
+
+const Dialogs = () => {
+    return (
+        <Main>
+            Dialogs
+        </Main>
+    );
+}
+
+export default Dialogs;
