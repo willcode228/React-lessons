2146dd99bd182b56639462d06f6e84c14d12dab8
Ilchenko Daniil Yurievich <crocus161@gmail.com>
2021-09-19 11:11:48 +0300

    Programming dialogs page

--- reactsamurai/src/components/Dialogs/Dialogs.jsx
+++ reactsamurai/src/components/Dialogs/Dialogs.jsx
@@ -1,9 +1,25 @@
+import s from './Dialogs.module.css'
 import { Main } from "../StyledComponents/Main";
+
 
 const Dialogs = () => {
     return (
-        <Main>
-            Dialogs
+        <Main className={s.dialogs}>
+
+            <ul className={s.dialogs__list}>
+                <li className={s.dialogs__item}>Valera</li>
+                <li className={s.dialogs__item}>Nikita</li>
+                <li className={s.dialogs__item}>Xuita</li>
+                <li className={s.dialogs__item}>Xulera</li>
+            </ul>
+
+            <ul className={s.messages}>
+                <li className={s.messages__item}>Hey</li>
+                <li className={s.messages__item}>Hey</li>
+                <li className={s.messages__item}>Hey</li>
+                <li className={s.messages__item}>Hey</li>
+            </ul>
+
         </Main>
     );
 }
